7e9a40914d14ce56f09b299eb74d556e9c2243fb
Cesar Guerrero <cesar.guerrero@robinpowered.com>
2022-09-01 16:31:57 -0400

    working with arrays

--- practice/app.js
+++ practice/app.js
@@ -1,5 +1,17 @@
+//TYPES 
+// any keyword can be used to any type to be assigned
+//ts version of speaclized object type
+//const person: {
+//    name: string;
+//    age: number;
+//} = {
 var person = {
     name: "Cesar",
-    age: 23
+    age: 23,
+    hobbies: ["Programming", "Reading"]
 };
 console.log(person);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- practice/app.ts
+++ practice/app.ts
@@ -1,12 +1,20 @@
+//TYPES 
+// any keyword can be used to any type to be assigned
+
 //ts version of speaclized object type
 //const person: {
 //    name: string;
 //    age: number;
 //} = {
-    
+
 const person = {
     name: "Cesar",
-    age: 23
+    age: 23,
+    hobbies: ["Programming", "Reading"]
 };
 
 console.log(person);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+}
