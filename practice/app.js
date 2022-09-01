500c37314b342327cebf7f03811a5048d1f3637e
Cesar Guerrero <cesar.guerrero@robinpowered.com>
2022-09-01 15:55:47 -0400

    2.11 using types

--- practice/app.js
+++ practice/app.js
@@ -1,1 +1,9 @@
-console.log('testing this');
+function add(n1, n2) {
+    return n1 + n2;
+}
+//this throws error
+//const x1 = '5';
+var x1 = 5;
+var x2 = 7;
+var result = add(x1, x2);
+console.log(result);
--- practice/app.ts
+++ practice/app.ts
@@ -1,1 +1,11 @@
-console.log('testing this');
+function add(n1: number, n2: number): number {
+    return n1 + n2;
+}
+
+//this throws error
+//const x1 = '5';
+const x1 = 5;
+const x2 = 7;
+
+const result = add(x1, x2);
+console.log(result);
