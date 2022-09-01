5bad3a0ca0f1845e547d2e68f1e6254758b6190d
Cesar Guerrero <cesar.guerrero@robinpowered.com>
2022-09-01 16:38:23 -0400

    tuples

--- practice/app.ts
+++ practice/app.ts
@@ -2,15 +2,16 @@
 // any keyword can be used to any type to be assigned
 
 //ts version of speaclized object type
-//const person: {
-//    name: string;
-//    age: number;
-//} = {
-
-const person = {
-    name: "Cesar",
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string]; //tuples with types in each position
+} = {
+    name: 'Cesar',
     age: 23,
-    hobbies: ["Programming", "Reading"]
+    hobbies: ['Programming', 'Reading'],
+    role: [1, 'Developer'] 
 };
 
 console.log(person);
