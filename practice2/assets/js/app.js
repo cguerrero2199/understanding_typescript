1e53bf3a9cdfdb8d4733e8470e80069b05fbd255
Cesar Guerrero <cesar.guerrero@robinpowered.com>
2022-09-02 14:38:50 -0400

    learning more about tsconfig

--- practice2/assets/js/app.js
+++ practice2/assets/js/app.js
@@ -1,5 +1,7 @@
 "use strict";
 //going to the root of the project and running tsc --init it will compile all ts files in the directory
 //can combine with --watch | -w to automatically compile all ts files after a change. 
-const testName = 'Cesar';
-console.log(testName);
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    console.log('Clicked!');
+});
--- practice2/assets/ts/app.ts
+++ practice2/assets/ts/app.ts
@@ -1,6 +1,8 @@
 //going to the root of the project and running tsc --init it will compile all ts files in the directory
 //can combine with --watch | -w to automatically compile all ts files after a change. 
 
-const testName = 'Cesar';
+const button = document.querySelector('button')!;
 
-console.log(testName);
+button.addEventListener('click', () => {
+    console.log('Clicked!');
+});
