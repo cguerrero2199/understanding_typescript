878d80e099e5b3c9bf94247ca0cb7944c00f6ab7
Cesar Guerrero <cesar.guerrero@robinpowered.com>
2022-09-02 14:22:52 -0400

    Working with confirguation file

---
+++ practice2/assets/ts/app.ts
@@ -0,0 +1,6 @@
+//going to the root of the project and running tsc --init it will compile all ts files in the directory
+//can combine with --watch | -w to automatically compile all ts files after a change. 
+
+const testName = 'Cesar';
+
+console.log(testName);
