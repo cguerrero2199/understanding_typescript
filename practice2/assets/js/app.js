f2cdcb9c5cb36340510e2230fe5451edcb825a00
Cesar Guerrero <cesar.guerrero@robinpowered.com>
2022-09-02 15:27:30 -0400

    Setting up debugger on vscode

--- practice2/assets/js/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-//going to the root of the project and running tsc --init it will compile all ts files in the directory
-//can combine with --watch | -w to automatically compile all ts files after a change. 
-const button = document.querySelector('button');
-button.addEventListener('click', () => {
-    console.log('Clicked!');
-});
-//# sourceMappingURL=app.js.map
